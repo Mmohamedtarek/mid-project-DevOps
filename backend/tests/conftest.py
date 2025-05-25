1ec18e1ce4ea3855541ab8b9146e1a4a0dcc423a
Mmohamedtarek <mo.tarek1042000@gmail.com>
2025-05-25 23:44:16 +0300

    pytest

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,15 +1,9 @@
-import os
 import pytest
 from app import app, db, Task
 
-
 @pytest.fixture(scope='module')
 def test_client():
-    database_url = os.getenv('DATABASE_URL')
-    if not database_url:
-        raise RuntimeError("DATABASE_URL environment variable is not set.")
-
-    app.config['SQLALCHEMY_DATABASE_URI'] = database_url
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
     app.config['TESTING'] = True
     app.config['WTF_CSRF_ENABLED'] = False
 
